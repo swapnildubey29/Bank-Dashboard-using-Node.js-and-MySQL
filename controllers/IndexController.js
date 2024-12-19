57b4b021873a55e13d270020d96f44e3d231c1bb
swapnildubey29 <swapnildubey3636@gmail.com>
2024-12-19 16:44:51 +0530

    updating login to MVC

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,37 +1,32 @@
 const db = require("../config/db")
 
 const signup = async (req, res) => {
- // console.log('Signup route hit');
+  // console.log('Signup route hit');
   const { name, email, password, confirmpassword } = req.body;
- // console.log('Data received:', { name, email, password });
-  const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+  // console.log('Data received:', { name, email, password });
+  const query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
 
   db.query(query, [name, email, password], (err, result) => {
-      if (err) {
-          console.error('Error inserting data:', err);
-          return res.status(500).send("Error saving data to database");
-      }
-      res.redirect('/dashboard')
-  })
-}
+    if (err) {
+      console.error("Error inserting data:", err)
+      return res.status(500).send("Error saving data to database")
+    }
+    res.redirect("/dashboard");
+  });
+};
 
-const login = async (req,res) =>{
-      const{email, password} = req.body
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-      const query = 'SELECT * FROM users WHERE email = ? AND password = ?';
+  const query = "SELECT * FROM users WHERE email = ? AND password = ?";
 
-      db.query(query, [email,password], (err, result) => {
-        if(err){
-          console.error('Error quering the database:', err)
-          return res.status(500).json({message: 'Database error'})
-        }
+  db.query(query, [email, password], (err, result) => {
+    if (err) {
+      console.error("Error quering the database:", err)
+      return res.status(500).json({ message: "Database error" })
+    }
+    res.redirect("/dashboard")
+  });
+};
 
-        if (result.length > 0) {
-          return res.json({ message: 'Login Successful', success: true })
-        } else {
-          return res.json({ message: 'Invalid Password', success: false })
-        }
-      })
-}
-
-module.exports = {signup, login}
+module.exports = { signup, login }
